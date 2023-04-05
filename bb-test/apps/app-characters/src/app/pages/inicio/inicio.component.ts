ae7589c6c00c16eaf851317af12626c8c93fb769
libardomo10 <lmedinaorjuela@gmail.com>
2023-04-05 17:00:39 -0500

    Ajuste estilos y acciones para seleccionar personajes.

--- bb-test/apps/app-characters/src/app/pages/inicio/inicio.component.ts
+++ bb-test/apps/app-characters/src/app/pages/inicio/inicio.component.ts
@@ -10,15 +10,32 @@ import { ServiceResponse } from '../../shared/interface/service-response';
 export class InicioComponent implements OnInit {
 
   public characters: Array<any> = [];
+  public listCharactersSelected: Array<any> = [];
+  public results!: number | undefined;
 
   constructor(private rickMortyService: RickMortyService) {}
 
   ngOnInit(): void {
+    this.listCharactersSelected = [];
     this.rickMortyService.getListCharacters().subscribe((response: ServiceResponse) => {
       console.log(response);
       console.log(response.info);
       this.characters = response.results;
+      this.results = response.info.count;
     });
   }
 
+  charactersSelected(id: number) {
+    let indexToRemove = this.characters.findIndex(resp => resp.id == id);
+    let charactersSelected = this.characters.splice(indexToRemove, 1);
+    this.listCharactersSelected.push(charactersSelected[0]);
+  }
+
+  removeCharacterSelected(id: number) {
+    let indexToRemove = this.listCharactersSelected.findIndex(resp => resp.id == id);
+    let characterToRemove = this.listCharactersSelected.splice(indexToRemove, 1);
+    console.log(characterToRemove);
+    
+    this.characters.push(characterToRemove[0]);
+  }
 }
